6c54b4f782801e4982d22250aacdd18cf4b841e0
Artur Oliveira <64819062+aetussilicon@users.noreply.github.com>
2025-08-02 11:50:26 -0300

    style: add colors palettes and funtion to get user preferred color scheme

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
+
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
+})
+
 </script>
 
 <template>
-  <div>
-    <h1 class="text-2xl">
+  <div class="bg-bg h-screen">
+    <h1 class="text-2xl text-text">
       Hello, world
     </h1>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,19 @@ export default {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: 'rgb(var(--color-bg) / <alpha-value>)',
+        card: 'rgb(var(--color-card) / <alpha-value>)',
+        'card-border': 'rgb(var(--color-card-border) / <alpha-value>)',
+        accent: 'rgb(var(--color-accent) / <alpha-value>)',
+        'accent-hover': 'rgb(var(--color-accent-hover) / <alpha-value>)',
+        'secondary-text': 'rgb(var(--color-secondary-text) / <alpha-value>)',
+        text: 'rgb(var(--color-text) / <alpha-value>)',
+      }
+    },
   },
   plugins: [],
 }
